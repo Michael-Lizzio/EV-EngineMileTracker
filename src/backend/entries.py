840b9021d5051a8cfb12cefa28e2383d6490be9b
Michael Lizzio <mjl2396@rit.edu>
2025-02-20 21:27:18 -0500

    Mod files

--- src/backend/entries.py
+++ src/backend/entries.py
@@ -6,19 +6,39 @@ entries_bp = Blueprint("entries", __name__)
 @entries_bp.route("/add_entry", methods=["POST"])
 def add_entry():
     data = request.json
-    required_fields = ["vehicle_id", "trip_odometer", "current_odometer", "gallons_added", "mpg", "date"]
-    
-    if not all(field in data for field in required_fields):
-        return jsonify({"message": "Missing required fields"}), 400
 
-    previous_entry = Entries.query.filter_by(vehicle_id=data["vehicle_id"]).order_by(Entries.current_odometer.desc()).first()
+    vehicle_id = data["vehicle_id"]
+    trip_odometer = data["trip_odometer"]
+    current_odometer = data["current_odometer"]
+    gallons_added = data["gallons_added"]
+    mpg = data["mpg"]
+    date = data["date"]
+
+    previous_entry = Entries.query.filter_by(vehicle_id=vehicle_id).order_by(Entries.current_odometer.desc()).first()
+
     previous_odometer = previous_entry.current_odometer if previous_entry else 0
 
-    if not data["current_odometer"]:
-        data["current_odometer"] = previous_odometer + data["trip_odometer"]
+    if current_odometer:
+        trip_odometer = current_odometer - previous_odometer
+    elif trip_odometer:
+        current_odometer = previous_odometer + trip_odometer
+    else:
+        return jsonify({"message": "Missing trip_odometer or current_odometer"}), 400  # Bad request
 
-    entry = Entries(**data)
-    db.session.add(entry)
-    db.session.commit()
+    if gallons_added and mpg and date:
+        entry = Entries(
+            vehicle_id = data["vehicle_id"],
+            trip_odometer = trip_odometer,
+            current_odometer = current_odometer,
+            gallons_added = data["gallons_added"],
+            mpg = data["mpg"],
+            date = data["date"]
+        )
 
-    return jsonify({"message": "Entry added successfully!"}), 201
+        db.session.add(entry)
+        db.session.commit()
+
+        return jsonify({"message": "Entry added successfully!"}), 201  # 201 Created
+
+    else:
+        return jsonify({"message": "Missing gallons_added or mpg or date"}), 400  # Bad request
